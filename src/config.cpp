c4c56bd01dfa2969f48c8d5a1143778929bb4508
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2017-10-16 07:05:56 +0100

    Listen on IPv6 by default

--- src/config.cpp
+++ src/config.cpp
@@ -8,11 +8,11 @@ using namespace std;
 using namespace boost::property_tree;
 
 Config::Config() : run_type(CLIENT),
-                   local_addr("127.0.0.1"),
+                   local_addr("::1"),
                    local_port(1080),
                    remote_addr(),
                    remote_port(443),
-                   password(),
+                   password("password"),
                    ca_certs(),
                    keyfile(),
                    certfile() {}
@@ -21,11 +21,11 @@ void Config::load(const string &filename) {
     ptree tree;
     read_json(filename, tree);
     run_type = (tree.get("run_type", string("client")) == "server") ? SERVER : CLIENT;
-    local_addr = tree.get("local_addr", string("127.0.0.1"));
+    local_addr = tree.get("local_addr", string("::1"));
     local_port = tree.get("local_port", uint16_t(1080));
     remote_addr = tree.get("remote_addr", string());
     remote_port = tree.get("remote_port", uint16_t(443));
-    password = tree.get("password", string());
+    password = tree.get("password", string("password"));
     ca_certs = tree.get("ca_certs", string());
     keyfile = tree.get("keyfile", string());
     certfile = tree.get("certfile", string());
