17fabb56fdcac2d918b6b561ee454dc3f687f0ff
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2017-10-23 14:37:13 -0700

    Server is optimized

--- src/serversession.cpp
+++ src/serversession.cpp
@@ -39,41 +39,196 @@ boost::asio::basic_socket<tcp, boost::asio::stream_socket_service<tcp> >& Server
 }
 
 void ServerSession::start() {
-
+    in_endpoint = in_socket.lowest_layer().remote_endpoint();
+    auto self = shared_from_this();
+    in_socket.async_handshake(stream_base::server, [this, self](const boost::system::error_code error) {
+        if (!error) {
+            in_async_read();
+        } else {
+            destroy();
+        }
+    });
 }
 
 void ServerSession::in_async_read() {
-
+    auto self = shared_from_this();
+    in_socket.async_read_some(boost::asio::buffer(in_read_buf, MAX_LENGTH), [this, self](const boost::system::error_code error, size_t length) {
+        if (!error) {
+            in_recv(string((const char*)in_read_buf, length));
+        } else {
+            destroy();
+        }
+    });
 }
 
 void ServerSession::in_async_write(const string &data) {
-
+    auto self = shared_from_this();
+    boost::asio::async_write(in_socket, boost::asio::buffer(data), [this, self](const boost::system::error_code error, size_t) {
+        if (!error) {
+            in_sent();
+        } else {
+            destroy();
+        }
+    });
 }
 
 void ServerSession::in_recv(const string &data) {
-
+    switch (status) {
+        case HANDSHAKE: {
+            auto self = shared_from_this();
+            size_t first = data.find("\r\n");
+            if (first != string::npos) {
+                if (config.password == data.substr(0, first)) {
+                    size_t second = data.find("\r\n", first + 2);
+                    if (second != string::npos) {
+                        string req_str = data.substr(first + 2, second - first - 2);
+                        TrojanRequest req;
+                        if (!req.parse(req_str)) {
+                            destroy();
+                            return;
+                        };
+                        status = CONNECTING_REMOTE;
+                        out_write_buf = data.substr(second + 2);
+                        tcp::resolver::query query(req.address, to_string(req.port));
+                        resolver.async_resolve(query, [this, self](const boost::system::error_code error, tcp::resolver::iterator iterator) {
+                            if (!error) {
+                                out_socket.async_connect(*iterator, [this, self](const boost::system::error_code error) {
+                                    if (!error) {
+                                        status = FORWARDING;
+                                        out_async_read();
+                                        if (out_write_buf != "") {
+                                            out_async_write(out_write_buf);
+                                        } else {
+                                            in_async_read();
+                                        }
+                                    } else {
+                                        destroy();
+                                    }
+                                });
+                            } else {
+                                destroy();
+                            }
+                        });
+                        return;
+                    }
+                }
+            }
+            status = CONNECTING_REMOTE;
+            out_write_buf = data;
+            tcp::resolver::query query(config.remote_addr, to_string(config.remote_port));
+            resolver.async_resolve(query, [this, self](const boost::system::error_code error, tcp::resolver::iterator iterator) {
+                if (!error) {
+                    out_socket.async_connect(*iterator, [this, self](const boost::system::error_code error) {
+                        if (!error) {
+                            status = FORWARDING;
+                            out_async_read();
+                            out_async_write(out_write_buf);
+                        } else {
+                            destroy();
+                        }
+                    });
+                } else {
+                    destroy();
+                }
+            });
+            break;
+        }
+        case CONNECTING_REMOTE: {
+            break;
+        }
+        case FORWARDING: {
+            out_async_write(data);
+            break;
+        }
+        case DESTROYING: {
+            break;
+        }
+    }
 }
 
 void ServerSession::in_sent() {
-
+    switch (status) {
+        case HANDSHAKE: {
+            break;
+        }
+        case CONNECTING_REMOTE: {
+            break;
+        }
+        case FORWARDING: {
+            out_async_read();
+            break;
+        }
+        case DESTROYING: {
+            break;
+        }
+    }
 }
 
 void ServerSession::out_async_read() {
-
+    auto self = shared_from_this();
+    out_socket.async_read_some(boost::asio::buffer(out_read_buf, MAX_LENGTH), [this, self](const boost::system::error_code error, size_t length) {
+        if (!error) {
+            out_recv(string((const char*)out_read_buf, length));
+        } else {
+            destroy();
+        }
+    });
 }
 
 void ServerSession::out_async_write(const string &data) {
-
+    auto self = shared_from_this();
+    boost::asio::async_write(out_socket, boost::asio::buffer(data), [this, self](const boost::system::error_code error, size_t) {
+        if (!error) {
+            out_sent();
+        } else {
+            destroy();
+        }
+    });
 }
 
 void ServerSession::out_recv(const string &data) {
-
+    switch (status) {
+        case HANDSHAKE: {
+            break;
+        }
+        case CONNECTING_REMOTE: {
+            break;
+        }
+        case FORWARDING: {
+            in_async_write(data);
+            break;
+        }
+        case DESTROYING: {
+            break;
+        }
+    }
 }
 
 void ServerSession::out_sent() {
-
+    switch (status) {
+        case HANDSHAKE: {
+            break;
+        }
+        case CONNECTING_REMOTE: {
+            break;
+        }
+        case FORWARDING: {
+            in_async_read();
+            break;
+        }
+        case DESTROYING: {
+            break;
+        }
+    }
 }
 
 void ServerSession::destroy() {
-
+    if (status == DESTROYING) {
+        return;
+    }
+    status = DESTROYING;
+    resolver.cancel();
+    out_socket.close();
+    auto self = shared_from_this();
+    in_socket.async_shutdown([this, self](const boost::system::error_code){});
 }
