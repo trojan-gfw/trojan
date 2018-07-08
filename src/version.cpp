403713ece2850cba076a2c92db5e7e10168db306
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-07-08 14:30:33 -0700

    Bump 1.4.4

--- src/version.cpp
+++ src/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.4.3");
+const string Version::version("1.4.4");
 
 string Version::get_version() {
     return version;
