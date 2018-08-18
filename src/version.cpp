8d43638655a5758b14a3b59eca25317c44ec95cc
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-08-17 20:55:25 -0700

    Bump 1.5.3

--- src/version.cpp
+++ src/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.5.2");
+const string Version::version("1.5.3");
 
 string Version::get_version() {
     return version;
