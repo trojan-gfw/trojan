f8da22db73ab41abdd0f32f947111eb38bb10dd7
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-10-17 15:58:25 -0700

    Bump 1.9.0

--- src/version.cpp
+++ src/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.8.1");
+const string Version::version("1.9.0");
 
 string Version::get_version() {
     return version;
