33b6b6b924d7d152fe5a6c1cfc30a2c0988eab59
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2019-09-29 22:32:54 -0700

    Bump 1.13.0

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.12.3");
+const string Version::version("1.13.0");
 
 string Version::get_version() {
     return version;
