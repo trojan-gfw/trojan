852719ecaba92471ba9fd3b4ea9925a19093ef4e
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2020-01-23 11:53:32 -0800

    Update copyright info

--- src/session/natsession.cpp
+++ src/session/natsession.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the trojan project.
  * Trojan is an unidentifiable mechanism that helps you bypass GFW.
- * Copyright (C) 2017-2019  GreaterFire
+ * Copyright (C) 2017-2020  The Trojan Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
