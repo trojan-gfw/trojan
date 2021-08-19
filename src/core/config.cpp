5bf40385536b1afe06b8f17172e9b2054a573b76
sheiy <xlz35429674@foxmail.com>
2021-08-19 20:19:33 +0800

    ...

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -65,8 +65,8 @@ void Config::populate(const ptree &tree) {
             password[SHA224(p)] = p;
         }
     }
-    Log::log_with_date_time("proxy_pass " + tree.get_child_optional("proxy_pass"), Log::INFO);
     if (tree.get_child_optional("proxy_pass")) {
+        Log::log_with_date_time("have proxy_pass", Log::INFO);
         proxy_pass_enabled = true;
         for (auto& item: tree.get_child("proxy_pass")) {
             string host = item.first;
