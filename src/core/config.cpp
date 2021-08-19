9a489b245415aaf029481c246f48d267adabc6a3
sheiy <xlz35429674@foxmail.com>
2021-08-19 20:32:51 +0800

    ...

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -67,7 +67,6 @@ void Config::populate(const ptree &tree) {
     }
     Log::log_with_date_time("try get proxy_pass config", Log::INFO);
     if (tree.get_child_optional("proxy_pass")) {
-        throw runtime_error("proxy_pass have");
         Log::log_with_date_time("have proxy_pass", Log::INFO);
         proxy_pass_enabled = true;
         for (auto& item: tree.get_child("proxy_pass")) {
